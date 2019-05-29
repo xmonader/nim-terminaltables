e97e5a7a5778085a38c57539c3ca76e9c606d28a
xmonader <xmonader@gmail.com>
2019-05-29 17:30:18 +0200

    update README and exmaples

--- src/terminaltables.nim
+++ src/terminaltables.nim
@@ -240,10 +240,15 @@ when isMainModule:
   
   t.reset()
 
-  let t2 = newUnicodeTable()
+  var t2 = newUnicodeTable()
   t2.separateRows = false
   t2.setHeaders(@[newCell("ID", pad=5), newCell("Name", rightpad=10), newCell("Fav animal", pad=2), newCell("Date", 5)])
   t2.addRow(@["1", "xmonader", "Cat, Dog", "2018-10-22"])
   t2.addRow(@["2", "ahmed", "Shark", "2015-12-6"])
   t2.addRow(@["3", "dr who", "Humans", "1018-5-2"])
+  printTable(t2)
+  t2.separateRows = false
+  let testStyle =  Style(rowSeparator:"┈", colSeparator:"┇", cellEdgeLeft:"├", cellEdgeRight:"┤", topLeft:"┏", topRight:"┓", bottomLeft:"└", bottomRight:"┘", topRowSeparator:"┬", bottomRowSeparator:"┴", dashLineLeft:"├", dashLineRight:"┤", dashLineColSeparatorLastRow:"┴", dashLineColSeparatorTopRow:"┬", dashLineColSeparator:"┼")
+
+  t2.style = testStyle
   printTable(t2)
